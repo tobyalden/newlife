8f9dd4542972bb633c77f233844ee4f2d21f0825
Toby Alden <toby.alden@gmail.com>
2023-08-02 13:45:05 -0700

    Use UUID urls for mixtapes

--- flaskr/mixtape.py
+++ flaskr/mixtape.py
@@ -6,13 +6,15 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
+import shortuuid
+
 bp = Blueprint('mixtape', __name__)
 
 @bp.route('/')
 def index():
     db = get_db()
     mixtapes = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, url, title, body, created, author_id, username'
         ' FROM mixtape p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -24,6 +26,7 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        url = get_uuid()
         error = None
 
         if not title:
@@ -34,14 +37,18 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO mixtape (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO mixtape (title, body, author_id, url)'
+                ' VALUES (?, ?, ?, ?)',
+                (title, body, g.user['id'], url)
             )
             db.commit()
             return redirect(url_for('mixtape.index'))
 
     return render_template('mixtape/create.html')
+
+def get_uuid():
+    # TODO: Ensure UUIDs are unique and don't conflict with other URLs
+    return shortuuid.ShortUUID().random(length=8)
 
 def get_mixtape(id, check_author=True):
     mixtape = get_db().execute(
@@ -59,15 +66,31 @@ def get_mixtape(id, check_author=True):
 
     return mixtape
 
-@bp.route('/<int:id>')
-def view(id):
-    mixtape = get_mixtape(id)
+def get_mixtape_by_url(url, check_author=True):
+    mixtape = get_db().execute(
+        'SELECT p.id, url, title, body, created, author_id, username'
+        ' FROM mixtape p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.url = ?',
+        (url,)
+    ).fetchone()
+
+    if mixtape is None:
+        abort(404, f"Mixtape with URL {url} doesn't exist.")
+
+    if check_author and (g.user is None or mixtape['author_id'] != g.user['id']):
+        abort(403)
+
+    return mixtape
+
+@bp.route('/<url>')
+def view(url):
+    mixtape = get_mixtape_by_url(url, False) # TODO: False here should be based on if the mix is public or not
     return render_template('mixtape/view.html', mixtape=mixtape)
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@bp.route('/<url>/update', methods=('GET', 'POST'))
 @login_required
-def update(id):
-    mixtape = get_mixtape(id)
+def update(url):
+    mixtape = get_mixtape_by_url(url)
 
     if request.method == 'POST':
         title = request.form['title']
@@ -84,7 +107,7 @@ def update(id):
             db.execute(
                 'UPDATE mixtape SET title = ?, body = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (title, body, mixtape['id'])
             )
             db.commit()
             return redirect(url_for('mixtape.index'))
