b8e5fcc8706c039b9e0dd6072e4bd19c19816192
Toby Alden <toby.alden@gmail.com>
2023-08-04 17:53:26 -0700

    Begin working on downloader and mixer

---
+++ flaskr/utils.py
@@ -0,0 +1,64 @@
+def download():
+    import os
+    import shutil
+    from yt_dlp import YoutubeDL
+    from pydub import AudioSegment
+
+    # shutil.rmtree('./youtube_rips')
+
+    # URLS = [
+        # 'https://www.youtube.com/watch?v=8RMDBqQDtT8',
+        # 'https://www.youtube.com/watch?v=xYNjxk6iIL0',
+        # 'https://www.youtube.com/watch?v=vb5Vlb9atEk',
+        # 'https://www.youtube.com/watch?v=dLX6reRrD88',
+        # 'https://www.youtube.com/watch?v=BLK3E5IkXgo',
+        # 'https://www.youtube.com/watch?v=3E_wHhW_vwE',
+    # ]
+
+    # ydl_opts = {
+        # 'paths': {
+            # 'home': './youtube_rips',
+            # 'temp': './temp',
+        # },
+        # 'format': 'm4a/bestaudio/best',
+        # # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        # 'postprocessors': [{  # Extract audio using ffmpeg
+            # 'key': 'FFmpegExtractAudio',
+            # 'preferredcodec': 'wav',
+        # }]
+    # }
+
+    # with YoutubeDL(ydl_opts) as ydl:
+        # ydl.download(URLS)
+
+    all_tracks = []
+    for filename in os.listdir('youtube_rips'):
+        if filename.endswith('.wav'):
+            track = AudioSegment.from_file('youtube_rips/' + filename)
+            all_tracks.append(track)
+
+    default_crossfade_time = 10 * 1000
+
+    mixed_tracks = None
+    for i, track in enumerate(all_tracks):
+        if mixed_tracks == None:
+            mixed_tracks = track
+        else:
+            last_track_length = len(all_tracks[i - 1])
+            next_track_length = len(all_tracks[i])
+            if min(last_track_length, next_track_length) < 30000:
+                # Don't crossfade in or out of tracks shorter than 30 seconds
+                crossfade_time = min(100, last_track_length, next_track_length)
+            else:
+                crossfade_time = min(
+                    default_crossfade_time,
+                    last_track_length / 3,
+                    next_track_length / 3,
+                )
+            mixed_tracks = mixed_tracks.append(track, crossfade=crossfade_time)
+
+    mixed_tracks.export("mix.mp3", format="mp3", bitrate="320k")
+
+download()
+# breakpoint()
+
